b0accf6680927f0e45d16da069dfee95201ce494
cym31153 <63576755+cym31153@users.noreply.github.com>
2023-04-08 20:51:35 +0800

    Update and rename web.sh to start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+# profile
+UUID=${UUID:-'7a296c9b-fb36-4d15-b552-e681d6fe94a4'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
